4339420c659c3547d489ae0038319e6acf656b29
Luke Blaney <git@lukeblaney.co.uk>
2025-01-08 00:38:18 +0000

    remove a bunch of unused paths and return 410 Gone for them

--- src/bbc/index.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-require("../backend/init.php");
-$backend=new Backend("Projects",false);
-
-$backend->addTitle("BBC");
-$backend->addOutput("<a href=\"/bbc/blogcomments\">Get Blog Comments RSS</a> (probably doesn't work since they changed their comments module)");
-$backend->addOutput("<br /><a href=\"/bbc/artisteps\">Artist Brands</a>");
-$backend->printit();
