88cd27e04ced7befb81f056d56f0261b728baf1b
Luke Blaney <git@lukeblaney.co.uk>
2025-01-10 01:48:26 +0000

    move /_info page to static json, rather than php

--- src/legacy-php/_info.php
+++
@@ -1,9 +0,0 @@
-<?php
-	header("Content-Type: application/json");
-	$output = [
-		"system" => "lukeblaney.co.uk",
-		"ci" => [
-			"circle" => "gh/lucas42/lukeblaney_co_uk",
-		],
-	];
-	echo json_encode($output);
